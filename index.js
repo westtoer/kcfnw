943efb22e345f7a59473df39627dd54db9aa96b0
Marc Portier <marc.portier@gmail.com>
2014-12-16 08:56:27 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+var argv = require('yargs')
+    .usage('Verwerk een netwerk-gebruik-enquete tot op het niveau dat je gebruik kunt plotten.\nUsage: $0')
+    .example('$0 -n 247', 'TODO describe what this example does.')
+    .describe('net', 'Het netwerk waarop het onderzoek betrekking heeft.')
+    .alias('n', 'net')       //long variant
+    .default('net', '126')     //default if any
+    .describe('work', 'Working directory to put data in.')
+    .alias('w', 'work')       
+    .default('work', '/tmp/kcfnw')
+    .describe('linkbase', 'Location of linkbase.csv to work with')
+    .alias('l','linkbase')
+    .default('linkbase', __dirname + '/data/base/linkbase.csv')
+    .describe('knoopbase', 'Location of knoopbase.csv to work with')
+    .alias('k','knoopbase')
+    .default('knoopbase', __dirname + '/data/base/knoopbase.csv')
+    .demand('n')
+    .argv;
+    
+var kcfnw=require('./lib/kcfnw');
+
+kcfnw.process(argv);
