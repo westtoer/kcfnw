b80a2eaa88d2204e65d0511e803667275c1baf83
Marc Portier <marc.portier@gmail.com>
2015-01-21 16:03:34 +0100

    minor cosmetics

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var kcfnw=require('./lib/kcfnw')
     .usage('Verwerk een netwerk-gebruik-enquete tot op het niveau dat je gebruik kunt plotten.\nUsage: $0')
     .example('$0 -n 247', 'TODO describe what this example does.')
     .describe('net', 'Het netwerk waarop het onderzoek betrekking heeft.')
-    .alias('n', 'net')       //long variant
+    .alias('n', 'net')         //long variant
     .default('net', '126')     //default if any
     .describe('work', 'Working directory to put data in.')
     .alias('w', 'work')       
