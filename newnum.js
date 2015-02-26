683cd3b01024be02dda868592f40e4f0fb2785d9
Marc Portier <MPT@w272.westtoer.be>
2015-02-26 16:02:39 +0100

    starting new feature in this realm

---
+++ newnum.js
@@ -0,0 +1,21 @@
+/*jslint node: true */
+/*jslint es5: true */
+/*jslint nomen: true */
+
+var renum = require('./lib/renum'),
+    path = require('path'),
+    argv = require('yargs')
+        .usage('Herberekend en allokeerd nummers voor optimale spreiding.\nUsage: $0')
+        .example('$0  -k data/renum/knoop.csv -l data/renum/link.csv', 'Will process the topology in the files.')
+        .describe('output', 'Output directory to put data in.')
+        .alias('o', 'output')
+        .default('o', '/tmp/renum')
+        .describe('linkbase', 'Location of linkbase.csv to work with')
+        .alias('l', 'linkbase')
+        .default('linkbase', path.join(__dirname, 'data', 'renum', 'link.csv'))
+        .describe('knoopbase', 'Location of knoopbase.csv to work with')
+        .alias('k', 'knoopbase')
+        .default('knoopbase', path.join(__dirname, 'data', 'renum', 'knoop.csv'))
+        .argv;
+
+renum.doWork(argv);
