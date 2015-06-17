fb3614e808872ca51d81fc394747533cb070f4ce
Marc Portier <MPT@w272.westtoer.be>
2015-06-17 15:11:41 +0200

    cosmetics

--- newnum.js
+++ newnum.js
@@ -7,27 +7,35 @@ var renum = require('./lib/renum'),
     argv = require('yargs')
         .usage('Herberekent en allokeerd nummers voor optimale spreiding.\nUsage: $0')
         .example('$0  -k data/renum/knoop.csv -l data/renum/link.csv', 'Will process the topology in the files.')
+
         .describe('output', 'Output directory to put data in.')
         .alias('o', 'output')
         .default('o', '/tmp/renum')
+
         .describe('linkbase', 'Location of linkbase.csv to work with')
         .alias('l', 'linkbase')
         .default('linkbase', path.join(__dirname, 'data', 'renum', 'link.csv'))
+
         .describe('knoopbase', 'Location of knoopbase.csv to work with')
         .alias('k', 'knoopbase')
         .default('knoopbase', path.join(__dirname, 'data', 'renum', 'knoop.csv'))
-        .describe('cutoff', 'the last known code for existing codes, al new ones should be bigger')
+
+        .describe('min', 'the shortest links that do not need reporting')
         .alias('m', 'min')
         .default('min', 0.059)
-        .describe('min', 'the shortest links that do not need reporting')
+
+        .describe('MAX', 'the longest links that do not need reporting')
         .alias('M', 'MAX')
         .default('MAX', 10.0)
-        .describe('MAX', 'the longest links that do not need reporting')
+
+        .describe('cutoff', 'the last known code for existing codes, al new ones should be bigger')
         .alias('c', 'cutoff')
         .default('cutoff', 'T0')
+
         .describe('spare', 'the numbers to spare and not use')
-        .alias('s', 'cutoff')
+        .alias('s', 'spare')
         .default('spare', '7,19,37,41,43,51,79,87,91')
+
         .argv;
 
 renum.doWork(argv);
